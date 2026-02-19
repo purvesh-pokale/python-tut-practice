c8627a606554a90d85413930f8b4229972bc94a6
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-02-18 20:40:06 -0700

    greater number

---
+++ conditional/if-else/greater-number.py
@@ -0,0 +1,17 @@
+#Take two numbers and print the greater number.
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+
+if a>b:
+    print(f"{a} is greater then {b}.")
+else:
+    print(f"{b} is greater then {a}.")
+
+'''
+o/p
+Enter the first number :15
+Enter the second number :10
+15 is greater then 10.
+
+
+'''
