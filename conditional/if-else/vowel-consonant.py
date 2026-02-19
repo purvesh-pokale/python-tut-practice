31b5b4ddcfaed2dd8bf57685efbb050902a6b9b2
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-02-18 21:06:09 -0700

    vowel-consonant

---
+++ conditional/if-else/vowel-consonant.py
@@ -0,0 +1,16 @@
+#Take a character and check if it is a vowel or consonant.
+
+cha = input("Enter the character :")
+
+if cha in "aeiouAEIOU":
+    print("Character is vowel.")
+else:
+    print("Character is consonant.")
+
+'''
+Enter the character :A
+Character is vowel.
+
+Enter the character :b
+Character is consonant.
+'''
