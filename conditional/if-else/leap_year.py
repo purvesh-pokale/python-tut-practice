26ec0eb467a2b456349413192244d5542fef3188
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-02-18 21:27:15 -0700

    leap year

--- conditional/if-else/leap_year.py
+++ conditional/if-else/leap_year.py
@@ -5,3 +5,12 @@ if year % 400 == 0 or(year%4==0 and year%100!=0):
     print("Leap year")
 else:
     print("not leep year")
+
+'''
+o/p
+Enter the year :254
+not leep year
+
+Enter the year :2028
+Leap year
+'''
