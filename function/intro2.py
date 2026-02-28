00b3aa69d98b6a3af4169887ada3dd65d59b4a74
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-02-28 10:56:12 -0700

    simple display  giveing parameter & argument not giving the return

---
+++ function/intro2.py
@@ -0,0 +1,21 @@
+def display_name(name):
+    print(f"Hello my name is {name}.")
+
+display_name("purvesh")
+
+#o/p= Hello my name is purvesh.
+
+def number(n):
+    for i in range(1,n+1):
+        if i%2==0:
+            print(i)
+number(10)
+
+'''
+o/p=
+2
+4
+6
+8
+10
+'''
